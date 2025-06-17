7d5f5536e215eb8531b27e26fd6c0632156beb36
Anton Marques Fedak <antonfedak@gmail.com>
2025-06-17 19:01:20 +0100

    Adding dynamic behavior to the navbar

--- calories/views.py
+++ calories/views.py
@@ -3,21 +3,24 @@ import requests
 import os
 import json
 
+names = []
+values = []
+
 # Create your views here.
 def calories(request):
+    global names, values
     if request.method == 'POST':
         query = request.POST['query'].strip()
         api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
         api_response = requests.get(api_url + query, headers={'X-Api-Key': os.environ['API_KEY']})
         try:
             data = json.loads(api_response.content)['items']
-            #print(data)
+            print(type(data))
+            names = list(data[0].keys())
+            values = list(data[0].values())
         except Exception as e:
-            data = "There was an error"
-            print(e)
+            data = {'message':"There was an error", 'query': query}
         return render(request, 'calories.html', {'data': data})
     else:
         return render(request, 'calories.html', {'data': ""})
     
-def performSearch(request):
-    pass
